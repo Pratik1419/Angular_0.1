194b389b9a3335e162d1b249cbbe97f82030d237
Pratik Mishra <pratikmishra141@gmail.com>
2024-10-01 13:34:57 +0530

    Update user.component.ts

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, computed, Input, Output, input, EventEmitter } from '@angular/core';
 
 
 
@@ -12,15 +12,22 @@ import { Component, Input } from '@angular/core';
   styleUrl: './user.component.css',
 })
 export class UserComponent {
- @Input() avatar!: string;
- @Input() name!: string;
-
+  @Input({required: true}) id!: string;
+ @Input({required: true}) avatar!: string;
+ @Input({required: true}) name!: string;
+ @Output() select = new EventEmitter();
 
  get imagePath() {
   return 'assets/users/' + this.avatar;
  }
+// avatar = input.required<string>();  //This input singles are only read-only signals.
+// name = input.required<string>();    //They can't change inside the component where the input is register.
+
+// imagePath = computed(() => {
+//   return 'assets/users/' + this.avatar();
+// });
 
   onSelectUser() {
-    
+    this.select.emit(this.id)
   }
 }
